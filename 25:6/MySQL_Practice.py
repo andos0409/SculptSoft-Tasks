5986706f24bcdd6d07fd89f0eb0b01a7a1e5b39a
Anhad <anhadsingh04@gmail.com>
2026-06-25 20:52:06 +1000

    Changed 25/6

--- 25:6/MySQL_Practice.py
+++ 25:6/MySQL_Practice.py
@@ -1,10 +1,12 @@
 import pymysql
+import os
+from dotenv import load_dotenv
 
 connection = pymysql.connect(
-    host = "localhost",
-    user = "root",
-    password = "H2.p1r8st",
-    database = "business_db"
+    host = os.getenv("DB_HOST"),
+    user = os.getenv("DB_USER"),
+    password = os.getenv("DB_PASSWORD"),
+    database = os.getenv("DB_NAME")
 )
 
 cursor = connection.cursor()
